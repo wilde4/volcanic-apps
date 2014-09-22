f6285fc4d544f40368b584bdbde9a2b9fa131ad9
Matt Ryder <m.ryder@volcanic.co.uk>
2014-09-22 16:26:42 +0100

    remove payment info from the routes

--- config/routes.rb
+++ config/routes.rb
@@ -26,8 +26,6 @@ Apps::Application.routes.draw do
     get "funds_earned"          => 'referral#funds_earned',         as: :referral_fee_earned
     get "funds_owed"            => 'referral#funds_owed',           as: :referral_fee_owed
     get "(/:id)/paid"           => 'referral#paid',                 as: :referral_paid
-    get "payment_form"          => 'referral#payment_form',         as: :referral_payment_new
-    patch "save_payment_info"   => 'referral#save_payment_info',    as: :referral_payment_save
     get 'referral_report'   => 'referral#referral_report',      as: :referral_report
     get 'notification_events' => 'referral#notification_events', as: :referral_notification_events
     post "activate_app"     => 'referral#activate_app',   as: :referral_activate_app
