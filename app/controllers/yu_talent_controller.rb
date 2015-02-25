569f965fff8769843d28bf84d473540f6fe7527c
Mufudzi Masaire <m.masaire@volcanic.co.uk>
2015-02-25 15:29:39 +0000

    adding logic for persisting user data, ie. in the save_user action of the YuTalentController

--- app/controllers/yu_talent_controller.rb
+++ app/controllers/yu_talent_controller.rb
@@ -9,6 +9,41 @@ class YuTalentController < ApplicationController
   end
 
   def save_user
+    @user = YuTalentUser.find_by(user_id: params[:user][:id])
+    if @user.present?
+      # update YuTalent user record
+      if @user.update(
+        email: params[:user][:email],
+        user_data: params[:user],
+        user_profile: params[:user_profile],
+        linkedin_profile: params[:linkedin_profile],
+        registration_answers: params[:registration_answer_hash]
+      )
+        logger.info "--- params = #{params.inspect}"
+        YuTalent::UserService.new(@user, params).post_user
+        render json: { success: true, user_id: @user.id }
+      else
+        render json: { success: false, status: "Error: #{@user.errors.full_messages.join(', ')}" }
+      end
+    else
+      # create new YuTalent user record
+      @user = YuTalentUser.new
+      @user.user_id = params[:user][:id]
+      @user.email = params[:user][:email]
+      @user.user_data = params[:user]
+      @user.user_profile = params[:user_profile]
+      @user.linkedin_profile = params[:linkedin_profile]
+      @user.registration_answers = params[:registration_answer_hash]
+
+      # persist user data
+      if @user.save
+        YuTalent::UserService.new(@user, params).post_user
+        render json: { success: true, user_id: @user.id }
+      else
+        render json: { success: false, status: "Error: #{@user.errors.full_messages.join(', ')}" }
+      end
+
+    end
   end
 
 end
