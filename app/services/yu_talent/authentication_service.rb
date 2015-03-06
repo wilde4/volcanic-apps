cdcb0e00096738e33b29b7c09e6fc3366fdea5cb
Mufudzi Masaire <m.masaire@volcanic.co.uk>
2015-03-06 14:45:54 +0000

    adding updated YuTalent::AuthenticationService

--- app/services/yu_talent/authentication_service.rb
+++ app/services/yu_talent/authentication_service.rb
@@ -3,46 +3,29 @@ class YuTalent::AuthenticationService < BaseService
 
   CREDENTIALS = {
     auth_url: 'https://yutalent.co.uk/c/oauth/authorize',
-    access_token_url: 'https://yutalent.co.uk/c/oauth/access_token',
-    redirect_url: 'http://localhost'
+    access_token_url: 'https://yutalent.co.uk/c/oauth/access_token'
   }
 
 
-  def initialize(params)
-    @params = params
-    @access_token = authenticate
+  def self.auth_url(dataset_id, app_id, host)
+    callback_url = "#{host}/admin/apps/#{app_id}/callback"
+    app_settings = AppSetting.find_by(dataset_id: dataset_id).try(:settings)
+    begin
+      client = OAuth2::Client.new(
+      '5454-9715A4F54-3984B43EE-8B6AF6793-24FC3',
+      'F6SIROFJVYECTZUNEQNDKK4M47QQURK1PEBIWGSH',
+        { authorize_url: CREDENTIALS[:auth_url], token_url: CREDENTIALS[:access_token_url] }
+      )
+      # client = OAuth2::Client.new(
+      #   app_settings[:client_id],
+      #   app_settings[:client_secret],
+      #   { authorize_url: CREDENTIALS[:auth_url], token_url: CREDENTIALS[:access_token_url] }
+      # )
+      auth_url = client.auth_code.authorize_url({ redirect_uri: callback_url, access_type: 'offline' })
+      return auth_url
+    rescue => e
+      puts e.inspect
+    end
   end
-
-
-  private
-
-
-    def authenticate
-      app_settings = YuTalentSetting.find_by(dataset_id: @params[:user][:dataset_id])
-      begin
-        client = OAuth2::Client.new(
-          app_settings[:client_id],
-          app_settings[:client_secret],
-          { authorize_url: CREDENTIALS[:auth_url], token_url: CREDENTIALS[:access_token_url] }
-        )
-        client.auth_code.authorize_url({ redirect_uri: CREDENTIALS[:redirect_url], access_type: 'offline' })
-        refresh = refresh_token
-        token = refresh.parsed_response['access_token'] if refresh.code == 200
-        access_token = OAuth2::AccessToken.from_hash(client, { access_token: token})
-      rescue => e
-        puts e.inspect
-      end
-    end
-
-
-    def refresh_token
-      options = {
-        body: { client_id: app_settings[:client_id], client_secret: app_settings[:client_secret], grant_type: 'refresh_token' },
-        headers: { 'Content-Type' => 'application/x-www-form-urlencoded' }
-      }
-      response = HTTParty.post(CREDENTIALS[:access_token_url], options)
-      return response
-    end
-
 
 end
