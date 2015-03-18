3aaec06648ee7c58499b1d4a1fc9c380f59f6e8e
Mufudzi Masaire <m.masaire@volcanic.co.uk>
2015-03-18 15:58:12 +0000

    updating Eventbrite::EventsService

--- app/services/eventbrite/events_service.rb
+++ app/services/eventbrite/events_service.rb
@@ -2,24 +2,20 @@ class Eventbrite::EventsService < BaseService
 
   class << self
 
-    def initialize(dataset_id)
-      @dataset_id = dataset_id
-      @client = EventBrite::AuthenticationService.client(@dataset_id)
-    end
-
-
-    def all_events
+    def all_events(dataset_id)
       begin
-        response = @client.user_list_events()
-        return response
+        @client = Eventbrite::AuthenticationService.client(dataset_id)
+        events = events_json_mapper(response)
+        return events
       rescue => e
         puts e.inspect
       end
     end
 
 
-    def search(query)
+    def search(dataset_id, query)
       begin
+        @client = Eventbrite::AuthenticationService.client(dataset_id)
         response = @client.event_search(query)
         events = events_json_mapper(response)
         return events
@@ -29,11 +25,12 @@ class Eventbrite::EventsService < BaseService
     end
 
 
-    def import_event(event_id)
+    def import_event(dataset_id, event_id)
       begin
+        @client = Eventbrite::AuthenticationService.client(dataset_id)
         response = @client.event_get({ id: event_id })
-        events = events_json_mapper(response)
-        return events
+        event = event_json_mapper(response)
+        return event
       rescue => e
         puts e.inspect
       end
@@ -51,15 +48,50 @@ class Eventbrite::EventsService < BaseService
             event = Hash.new
             event["id"] = e["event"]["id"]
             event["title"] = e["event"]["title"]
+            event["description"] = e["event"]["description"]
             event["url"] = e["event"]["url"]
             event["logo"] = e["event"]["logo"]
-            event[:start_date_time] = date_parser(e["event"]["start_date"], e["event"]["timezone"])
-            event[:end_date_time] = date_parser(e["event"]["end_date"], e["event"]["timezone"])
-            event[:publish_date] = date_parser(e["event"]["created"], e["event"]["timezone"])
+            event["status"] = e["event"]["status"]
+            event["privacy"] = e["event"]["privacy"]
+            event["num_attendee_rows"] = e["event"]["num_attendee_rows"]
+            event["tags"] = e["event"]["tags"]
+            event["venue"] = e["event"]["venue"]
+            event["tickets"] = e["event"]["tickets"]
+            event["organizer"] = e["event"]["organizer"]
+            event["start_date_time"] = date_parser(e["event"]["start_date"], e["event"]["timezone"])
+            event["end_date_time"] = date_parser(e["event"]["end_date"], e["event"]["timezone"])
+            event["publish_date"] = date_parser(e["event"]["created"], e["event"]["timezone"])
             all_events << event
           end
         end
-        return all_events.to_json
+        return all_events
+      end
+
+
+      def event_json_mapper(response)
+        return if response["event"].nil?
+        e = response["event"]
+        event = Hash.new
+        event_hash = Array.new
+        if e
+          event["id"] = e["id"]
+          event["title"] = e["title"]
+          event["description"] = e["description"]
+          event["url"] = e["url"]
+          event["logo"] = e["logo"]
+          event["status"] = e["status"]
+          event["privacy"] = e["privacy"]
+          event["num_attendee_rows"] = e["num_attendee_rows"]
+          event["tags"] = e["tags"]
+          event["venue"] = e["venue"]
+          event["tickets"] = e["tickets"]
+          event["organizer"] = e["organizer"]
+          event["start_date_time"] = date_parser(e["start_date"], e["timezone"])
+          event["end_date_time"] = date_parser(e["end_date"], e["timezone"])
+          event["publish_date"] = date_parser(e["created"], e["timezone"])
+          event_hash << event
+        end
+        return event_hash
       end
 
 
