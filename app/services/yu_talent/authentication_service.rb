82fa8fea0212f7e5578f23953a37b6972d205edd
Mufudzi Masaire <m.masaire@volcanic.co.uk>
2015-03-23 16:21:11 +0000

    clean up

--- app/services/yu_talent/authentication_service.rb
+++ app/services/yu_talent/authentication_service.rb
@@ -10,19 +10,13 @@ class YuTalent::AuthenticationService < BaseService
       access_token_url: 'https://yutalent.co.uk/c/oauth/access_token'
     }
 
-
     def client
       begin
         client = OAuth2::Client.new(
-        '5454-9715A4F54-3984B43EE-8B6AF6793-24FC3',
-        'F6SIROFJVYECTZUNEQNDKK4M47QQURK1PEBIWGSH',
+          ENV["YU_TALENT_CLIENT_ID"],
+          ENV["YU_TALENT_CLIENT_SECRET"],
           { authorize_url: CREDENTIALS[:auth_url], token_url: CREDENTIALS[:access_token_url] }
         )
-        # client = OAuth2::Client.new(
-        #   ENV["YU_TALENT_CLIENT_ID"],
-        #   ENV["YU_TALENT_CLIENT_SECRET"],
-        #   { authorize_url: CREDENTIALS[:auth_url], token_url: CREDENTIALS[:access_token_url] }
-        # )
         return client
       rescue => e
         puts e.inspect
@@ -52,13 +46,11 @@ class YuTalent::AuthenticationService < BaseService
 
 
     def get_access_token(refresh_token)
-      # begin
+      begin
         options = {
           body: {
-            client_id: '5454-9715A4F54-3984B43EE-8B6AF6793-24FC3',
-            client_secret: 'F6SIROFJVYECTZUNEQNDKK4M47QQURK1PEBIWGSH',
-            # client_id: ENV["YU_TALENT_CLIENT_ID"],
-            # client_secret: ENV["YU_TALENT_CLIENT_SECRET"],
+            client_id: ENV["YU_TALENT_CLIENT_ID"],
+            client_secret: ENV["YU_TALENT_CLIENT_SECRET"],
             refresh_token: refresh_token,
             grant_type: 'refresh_token'
           },
@@ -76,9 +68,9 @@ class YuTalent::AuthenticationService < BaseService
           puts "Whoops, couldn't retrieve access token with stored refresh token."
           @access_token = nil
         end
-      # rescue => e
-      #   puts e.inspect
-      # end
+      rescue => e
+        puts e.inspect
+      end
     end
 
 
