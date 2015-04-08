730f35d8fd9701483ce6dd47d4a4cea862de0f0d
Mufudzi Masaire <m.masaire@volcanic.co.uk>
2015-04-08 12:59:55 +0100

    refactoring yu:talent authentication service

--- app/services/yu_talent/authentication_service.rb
+++ app/services/yu_talent/authentication_service.rb
@@ -26,9 +26,8 @@ class YuTalent::AuthenticationService < BaseService
 
     def auth_url(app_id, host)
       begin
-        @host = format_url(host)
-        callback_url = "#{@host}/admin/apps/#{app_id}/callback"
-        auth_url = client.auth_code.authorize_url({ redirect_uri: callback_url, access_type: 'offline' })
+        @callback_url = callback_url(app_id, host)
+        auth_url = client.auth_code.authorize_url({ redirect_uri: @callback_url, access_type: 'offline' })
         @auth_url = URI.decode(auth_url)
         return @auth_url
       rescue => e
@@ -37,26 +36,31 @@ class YuTalent::AuthenticationService < BaseService
     end
 
 
-    def format_url(url)
-      url = URI.parse(url)
-      return url if url.scheme
-      return "http://#{url}:3000" if Rails.env.development?
-      "http://#{url}"
-    end
-
-
-    def get_access_token(dataset_id)
+    def get_access_token(app_id, host, authorization_code)
       begin
-        @authorization_code = YuTalentAppSetting.find_by(dataset_id: dataset_id).try(:authorization_code)
-        @host = Key.find_by(app_dataset_id: dataset_id).try(:host)
-        @callback_url = format_url(@host)
-        @token = client.auth_code.get_token(@authorization_code, redirect_uri: @callback_url)
-        return @token
+        @callback_url = callback_url(app_id, host)
+        @token = client.auth_code.get_token(authorization_code, redirect_uri: @callback_url)
+        @token
       rescue => e
         puts e.inspect
       end
     end
 
+
+    private
+
+      def callback_url(app_id, host)
+        @host = format_url(host)
+        "#{@host}/admin/apps/#{app_id}/callback"
+      end
+
+
+      def format_url(url)
+        url = URI.parse(url)
+        return url if url.scheme
+        return "http://#{url}:3000" if Rails.env.development?
+        "http://#{url}"
+      end
 
   end
 
