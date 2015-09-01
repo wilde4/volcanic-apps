4fb893e19a80b42acea045e95fb65bad5f233800
Matthew Whiteley <m.whiteley@volcanic.co.uk>
2015-09-01 16:03:30 +0100

    default fallbacks if title not specified

--- app/models/job_board.rb
+++ app/models/job_board.rb
@@ -1,4 +1,13 @@
 class JobBoard < ActiveRecord::Base
   validates_presence_of :app_dataset_id
 
+
+
+  def job_token_title
+    self.read_attribute(:job_token_title).present? ? self.read_attribute(:job_token_title) : "Job Credit"
+  end
+
+  def cv_search_title
+    self.read_attribute(:cv_search_title).present? ? self.read_attribute(:cv_search_title) : "CV Search Access"
+  end
 end
