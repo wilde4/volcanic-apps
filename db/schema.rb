0b34e33ea319f0b8e1ded93cca9f503acfa48103
Ben Gibbs <ben@bobop.co.uk>
2015-10-30 16:19:54 +0000

    schema change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029113445) do
+ActiveRecord::Schema.define(version: 20151030134920) do
 
   create_table "achievements", force: true do |t|
     t.integer "user_id"
