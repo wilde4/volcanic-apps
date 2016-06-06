31a301b63eeb2a8c1fb8973e450b2f169ee1d23d
jccanizares <juancarloscanizares@gmail.com>
2016-06-06 16:25:10 -0500

    connect to mailchimp login for oauth2

--- app/services/mail_chimp/authentication_service.rb
+++ app/services/mail_chimp/authentication_service.rb
@@ -6,19 +6,19 @@ class MailChimp::AuthenticationService < BaseService
   class << self
 
     CREDENTIALS = {
-      auth_url: 'https://login.mailchimp.com/oauth2/authorize',
-      access_token_url: 'https://login.mailchimp.com/oauth2/token',
+      authorize_url: 'https://login.mailchimp.com/oauth2/authorize',
+      token_url: 'https://login.mailchimp.com/oauth2/token',
       client_id: '651907686702',
       client_secret: 'b5e3a197a0e465d2f0e42a7dba8b9393'
     }
     
 
     def client_auth(app_id, host)
-      client = OAuth2::Client.new(CREDENTIALS[:client_id], CREDENTIALS[:client_secret], token_url: CREDENTIALS[:access_token_url], site: CREDENTIALS[:auth_url])
+      client = OAuth2::Client.new(CREDENTIALS[:client_id], CREDENTIALS[:client_secret], { authorize_url: CREDENTIALS[:authorize_url], token_url: CREDENTIALS[:token_url] })
       authorize_url = client.auth_code.authorize_url(redirect_uri: redirect_uri(app_id, host), response_type: 'code')
 
-      # authorize_url.sub! 'oauth', 'oauth2'
-      return authorize_url
+      return URI.decode(authorize_url)
+      
     end
     
 
