863f80ba6d71d0dd809ed24cf649db3fb3416394
jccanizares <juancarloscanizares@gmail.com>
2016-06-21 21:03:59 -0500

    set semrush key in application.yml

--- config/initializers/semrush.rb
+++ config/initializers/semrush.rb
@@ -1,4 +1,3 @@
 Semrush.config do |config|
-  # config.api_key = "b57707c3971fe2be0fd86454f0bde7c3" #previous api key
-  config.api_key = "407c7ccbe52c01cc59c32fb1652201f6"
+  config.api_key = ENV['SEMRUSH_KEY']
 end
