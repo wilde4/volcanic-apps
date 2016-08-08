1693eca0ba946aad57740d19aabce7310f4cdac5
Andy Turner <a.turner@volcanic.co.uk>
2016-08-08 12:27:26 +0100

    Bond Adapt app de/activation routes

--- config/routes.rb
+++ config/routes.rb
@@ -236,6 +236,8 @@ Apps::Application.routes.draw do
   end
 
   scope :bond_adapt do
+    post "activate_app"     => 'bond_adapt#activate_app'
+    post "deactivate_app"   => 'bond_adapt#deactivate_app'
     get  "index"            => 'bond_adapt#index'
     post "save_user"        => 'bond_adapt#save_user'
   end
