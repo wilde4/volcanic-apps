aadf4feaaa95f5eca2f0dcb2f859aa512777973f
Stephen Hatton <s.hatton@volcanic.co.uk>
2017-03-08 15:37:55 +0000

    - Removed some commented out code

--- app/controllers/candidate_sharing_controller.rb
+++ app/controllers/candidate_sharing_controller.rb
@@ -37,7 +37,6 @@ class CandidateSharingController < ApplicationController
     if params[:user].present? && params[:user][:extra].present? and params[:user][:extra][:filtered_notifications].present?
       client_ids = params[:user][:extra][:filtered_notifications][:client_ids]
       if client_ids.is_a?(Array)
-        # FilteredNotificationSending.create(job_id: params[:job][:id], client_ids: client_ids)
         response = post_to_api("notifications", "trigger_clients_notification", {client_ids: client_ids, notification: "filtered_candidate_announcement", user_id: params[:user][:id]})
       end   
     end
