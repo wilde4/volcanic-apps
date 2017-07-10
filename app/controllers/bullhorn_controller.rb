496aa3b532a319a27a6c05fc78b8482ad85e377f
Andy Turner <a.turner@volcanic.co.uk>
2017-07-10 16:41:37 +0100

    No more deleting bullhorn app settings when deactivating to assist with transition to v2 app

--- app/controllers/bullhorn_controller.rb
+++ app/controllers/bullhorn_controller.rb
@@ -10,19 +10,6 @@ class BullhornController < ApplicationController
 
   # To Authorize a Bullhorn API user, follow instruction on https://github.com/bobop/bullhorn-rest
 
-  def deactivate_app
-    key = Key.where(app_dataset_id: params[:data][:app_dataset_id], app_name: params[:controller]).first
-    respond_to do |format|
-      if key
-        bullhorn_setting = BullhornAppSetting.find_by(dataset_id: params[:data][:app_dataset_id])
-        bullhorn_setting.destroy if bullhorn_setting
-        format.json { render json: { success: key.destroy }}
-      else
-        format.json { render json: { error: 'Key not found.' } }
-      end
-    end
-  end
-
   def index
     @bullhorn_setting = BullhornAppSetting.find_by(dataset_id: params[:data][:dataset_id]) || BullhornAppSetting.new(dataset_id: params[:data][:dataset_id])
     get_fields(params[:data][:dataset_id]) if @bullhorn_setting.authorised?
