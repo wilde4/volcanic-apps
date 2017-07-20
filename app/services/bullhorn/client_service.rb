cfe994742fcc9355c1886b0fddc1a957b62b99a7
Andy Turner <a.turner@volcanic.co.uk>
2017-07-20 22:49:01 +0100

    parse bullhorn dateEnd timestamp into date

--- app/services/bullhorn/client_service.rb
+++ app/services/bullhorn/client_service.rb
@@ -346,6 +346,8 @@ class Bullhorn::ClientService < BaseService
                 sectors << b_sector.data.name.strip
               end
               value = sectors.join(',')
+            when 'dateEnd'
+              value = Time.at(job.dateEnd / 1000).to_date.to_s
             else
               bullhorn_value = job.send(fm.bullhorn_field_name)
               if bullhorn_value.is_a? Hash
