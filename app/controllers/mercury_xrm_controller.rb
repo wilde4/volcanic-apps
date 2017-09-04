359f08fcb9190d720838a35950bcec15bcb43766
Stephen Hatton <s.hatton@volcanic.co.uk>
2017-09-04 16:21:03 +0100

    # Some work on Mercury controller # Updated Mercury XRM data-key (as per Shabaaz’s email)

--- app/controllers/mercury_xrm_controller.rb
+++ app/controllers/mercury_xrm_controller.rb
@@ -3,12 +3,13 @@ class MercuryXrmController < ApplicationController
   layout "no_jquery_application"
 
   def mercury_xrm_dashboard
-    @encrypted_email = _encrypt_email(params[:data][:email])
+    @encrypted_email = encrypt_email(params[:data][:email])
     render "mercury_xrm_dashboard"
   end
 
+  private
 
-  def _encrypt_email(email)
+  def encrypt_email(email)
     return "" if email.blank?
     return AesEncryptionService.encrypt_email(email)
   end
@@ -18,5 +19,5 @@ class MercuryXrmController < ApplicationController
   # which constructs a HTTP GET request to Volcanic Apps server, passing the current_users email
   # once VA receives this request: then it performs some logic
   # and renders/returns a view which is then displayed on the user dashboard
-
+  
 end
